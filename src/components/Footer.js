28f3622a16d48d5fa71c892b14169685481fcf03
YongHo <qkaxhf8823@naver.com>
2023-06-21 04:46:29 +0900

    MainPage & Login Page

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <div>
+      <div
+        style={{
+          backgroundColorcolor: 'white',
+          zIndex: '10000',
+          width: '100%',
+        }}
+      >
+        fsdlkjasdlksad
+        <div>dsassssss</div>
+      </div>
+    </div>
+  );
+}
+export default Footer;
